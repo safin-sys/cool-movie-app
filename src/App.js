fdaf9243d00ad427ddfef61a9343cf2b08311358
Safin Ahmed <61120114+safin-sys@users.noreply.github.com>
2020-12-21 21:51:55 +0600

    router too hard

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import Carousels from "./components/Carousels";
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Search from './components/Search';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import MovieDetails from './components/MovieDetails';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/md" component={MovieDetails} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+function Home() {
+  return (
+    <React.Fragment>
       <Hero />
       <Search />
       <Carousels />
-    </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ham from '../img/icons/hamburger.svg';
 import x from '../img/icons/x.svg';
+import {Link} from 'react-router-dom';
 
 function Navbar() {
     const [hamNavStatus, setHamNavStatus] = useState(false);
@@ -19,18 +20,20 @@ function Navbar() {
             <nav>
                 <a className="logo" href="/"><h1>COOL <br/> MOVIEAPP</h1></a>
                 <ul>
-                    <li><a href="/" className="nav-active">HOME</a></li>
+                    <li><Link to="/" className="nav-active">HOME</Link></li>
                     <li><a href="#upcoming">UPCOMING</a></li>
                     <li><a href="#popular">POPULAR</a></li>
+                    <li><Link to="/md">MOVIE DETAILS</Link></li>
                 </ul>
                 <button onClick={handleHam} className="ham"><img src={ham} alt="HAM"/></button>
             </nav>
             <div className="ham-nav-menu">
                 <button onClick={handleHam} className="ham"><img src={x} alt="HAM"/></button>
                 <ul>
-                    <li><a href="/" className="nav-active">HOME</a></li>
+                    <li><Link to="/" className="nav-active">HOME</Link></li>
                     <li><a href="#upcoming">UPCOMING</a></li>
                     <li><a href="#popular">POPULAR</a></li>
+                    <li><Link to="/md">MOVIE DETAILS</Link></li>
                 </ul>
             </div>
         </React.Fragment>
