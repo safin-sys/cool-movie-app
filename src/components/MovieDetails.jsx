c106c9f18cfa17ff281341951eb1732468f8aea7
Safin Ahmed <61120114+safin-sys@users.noreply.github.com>
2020-12-27 21:12:54 +0600

    movie details page videos added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import ratingIcon from '../img/icons/rating.svg'
+import ratingIcon from '../img/icons/rating.svg';
+import play from '../img/icons/play.svg';
 
 function MovieDetails({ match }) {
     const [movie, setMovie] = useState({});
@@ -26,10 +27,11 @@ function MovieDetails({ match }) {
                 </div>
 
                 <div className="md-hero__info">
-                    {movie.poster_path ? <img className="md-hero__info__poster" src={imgUrl + movie.poster_path} alt={movie.original_title + 'Poster'} /> : null}
+                    {movie.poster_path ? <img className="md-hero__info__poster" src={'https://image.tmdb.org/t/p/w200' + movie.poster_path} alt={movie.original_title + 'Poster'} /> : null}
 
                     <div className="md-hero__info__overview">
                         <h1 className="md-hero__info__overview__title">{movie.original_title || movie.original_name}</h1>
+                        <p className="md-hero__info__overview__tagline">{movie.tagline}</p>
                         
                         <div className="md-hero__info__overview__detail">
                             <p className="md-hero__info__overview__detail__run_time">{movie.runtime} min </p>
@@ -46,7 +48,7 @@ function MovieDetails({ match }) {
 
                         <div className="md-hero__info__overview__rating">
                             <img src={ratingIcon} alt="Rating Icon"/>
-                            <p>{movie.vote_average}</p>
+                            <p>{movie.vote_average} <span>({movie.vote_count} votes)</span></p>
                         </div>
                     </div>
                 </div>
@@ -56,8 +58,57 @@ function MovieDetails({ match }) {
                 <h3>Plot Summary</h3>
                 <p>{movie.overview}</p>
             </div>
+            <div className="md-details">
+                <MovieVideos id={match.params.id} type={match.params.type} />
+                <MovieCasts id={match.params.id} type={match.params.type} />
+            </div>
         </React.Fragment>
     )
 }
 
 export default MovieDetails;
+
+function MovieVideos({id, type}) {
+    const url = `https://api.themoviedb.org/3/${type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}`;
+
+    const [vids, setVids] = useState([]);
+
+    useEffect(() => {
+        async function getVids() {
+            const res = await fetch(url);
+            const data = await res.json();
+
+            setVids(data.results);
+        };
+
+        getVids();
+    }, [url])
+    return (
+        <div className="videos">
+            <h3>Videos</h3>
+            <div className="md-vid">
+                {vids.map((vid, i) => {
+                    return (
+                        <div className="vid-card" key={i}>
+                            <div className="thumbnail"
+                            style={{
+                                backgroundImage: `linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(0,212,255,0) 60%),url(https://img.youtube.com/vi/${vid.key}/0.jpg)`
+                            }}></div>
+                
+                            <a href={`https://youtu.be/${vid.key}`} target="_blank" rel="noreferrer"><img src={play} alt="Play Button"/></a>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+function MovieCasts(id, type) {
+    return (
+        <div className="casts">
+            <h3>Casts</h3>
+            <p>Coming Soon...</p>
+        </div>
+    )
+}
